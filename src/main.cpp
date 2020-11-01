7a3f1465a2dea6000656b20404c7955dcdd4aba9
Ole Martin Brende <olemartin87@gmail.com>
2020-11-01 14:55:09 +0100

    On-off-on button support

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,11 @@
 
 //DEFINITIONS
 #define ENABLE_PULLUPS
-#define NUMBUTTONS 9 //replace "?"with number of buttong you are using
+#define NUMBUTTONS 10 //replace "?"with number of buttong you are using
 #define NUMROWS 3 //replace "?" with number of rows you have
 #define NUMCOLS 3 //replace "?" with number of columns you have
 
-
+void CheckTriState(void);
 void CheckAllButtons(void);
 
 //BUTTON MATRIX
@@ -16,13 +16,13 @@ void CheckAllButtons(void);
 //then replace all "?" with numbers (starting from 0)
 byte buttons[NUMROWS][NUMCOLS] = {
   {1,2,3},
-  {4,9,5},
-  {6,7,8}
+  {4,5,6},
+  {7,8,9}
 };
 
 //BUTTON MATRIX PART 2
-byte rowPins[NUMROWS] = {6,7,8}; //change "?" to the pins the rows of your button matrix are connected to
-byte colPins[NUMCOLS] = {3,4,5}; //change "?" to the pins the rows of your button matrix are connected to
+byte rowPins[NUMROWS] = {5,6,7}; //change "?" to the pins the rows of your button matrix are connected to
+byte colPins[NUMCOLS] = {2,3,4}; //change "?" to the pins the rows of your button matrix are connected to
 
 Keypad buttbx = Keypad( makeKeymap(buttons), rowPins, colPins, NUMROWS, NUMCOLS);
 
@@ -31,11 +31,11 @@ Keypad buttbx = Keypad( makeKeymap(buttons), rowPins, colPins, NUMROWS, NUMCOLS)
 //JOYSTICK SETTINGS
 Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,
   JOYSTICK_TYPE_JOYSTICK,
-  9, //number of buttons
+  13, //number of buttons
   0, //number of hat switches
   //Set as many axis to "true" as you have potentiometers for
-  true, // y axis
-  true, // x axis
+  false, // y axis
+  false, // x axis
   false, // z axis
   false, // rx axis
   false, // ry axis
@@ -47,43 +47,41 @@ Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,
   false); // steering wheel
 
 // Constant that maps the physical pin to the joystick button.
-const int pinToButtonMap = 2;
-const int XPin = A5;
-const int YPin = A4;
-int range = 1023;
-int center = 0;
+const int pinToButtonMap1 = 8;
+const int pinToButtonMap2 = 9;
+//const int XPin = A5;
+//const int YPin = A4;
+//int range = 1023;
+//int center = 0;
 
 void setup() {
 	// Initialize Button Pins
-	pinMode(pinToButtonMap, INPUT_PULLUP);
-  pinMode(XPin, INPUT);
-  pinMode(YPin, INPUT);
+	pinMode(pinToButtonMap1, INPUT);
+	pinMode(pinToButtonMap2, INPUT);
+	// pinMode(XPin, INPUT);
+	// pinMode(YPin, INPUT);
 
 	// Initialize Joystick Library
 	Joystick.begin();
 }
 
 // Last state of the button
-int lastButtonState = 0;
+int lastButtonState1 = 0;
+int lastButtonState2 = 0;
+int lastButtonState3 = 1;
 
 void loop() {
 
-	// Read pin values
-	int currentButtonState = !digitalRead(pinToButtonMap);
-	if (currentButtonState != lastButtonState){
-	  Joystick.setButton(0, currentButtonState);
-	  lastButtonState = currentButtonState;
-	}
-
+	CheckTriState();
 	CheckAllButtons();
 
-  double xpin_value = analogRead(XPin);
-  xpin_value = map(xpin_value, 0, 1023, 0, range);
-  Joystick.setXAxis(xpin_value);
+	// double xpin_value = analogRead(XPin);
+	// xpin_value = map(xpin_value, 0, 1023, 0, range);
+	// Joystick.setXAxis(xpin_value);
 
-  double ypin_value = analogRead(YPin);
-  ypin_value = map(ypin_value, 0, 1023, 0, range);
-  Joystick.setYAxis(ypin_value);
+	// double ypin_value = analogRead(YPin);
+	// ypin_value = map(ypin_value, 0, 1023, 0, range);
+	// Joystick.setYAxis(ypin_value);
 
 	delay(50);
 }
@@ -105,4 +103,54 @@ void CheckAllButtons(void){
 			}   
 		}
 	}
+}
+
+void CheckTriState(void){
+
+	bool set_once = true;
+
+	if (set_once) {
+		// Read pin values
+		int currentButtonState1 = digitalRead(pinToButtonMap1);
+		if (currentButtonState1 != lastButtonState1){	
+			lastButtonState1 = currentButtonState1;
+			Joystick.setButton(11, currentButtonState1);
+			delay(50);
+			Joystick.setButton(11, 0);
+		}
+
+		int currentButtonState2 = digitalRead(pinToButtonMap2);
+		if (currentButtonState2 != lastButtonState2){
+			lastButtonState2 = currentButtonState2;
+			Joystick.setButton(10, currentButtonState2);
+			delay(50);
+			Joystick.setButton(10, 0);
+		}
+		// Central button is true of both others are false
+		int currentButtonState3 = !currentButtonState1 && !currentButtonState2;
+		if (currentButtonState3 != lastButtonState3){
+			lastButtonState3 = currentButtonState3;
+			Joystick.setButton(12, currentButtonState3);
+			delay(50);
+			Joystick.setButton(12, 0);
+		}
+	}else{
+		// Read pin values
+		int currentButtonState1 = digitalRead(pinToButtonMap1);
+		if (currentButtonState1 != lastButtonState1){
+		Joystick.setButton(11, currentButtonState1);
+		lastButtonState1 = currentButtonState1;
+		}
+
+		int currentButtonState2 = digitalRead(pinToButtonMap2);
+		if (currentButtonState2 != lastButtonState2){
+		Joystick.setButton(10, currentButtonState2);
+		lastButtonState2 = currentButtonState2;
+		}
+
+		if (!currentButtonState1 && !currentButtonState2) {
+			Joystick.setButton(12, true);
+		}
+	}
+
 }
